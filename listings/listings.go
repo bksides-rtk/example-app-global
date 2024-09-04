4f9944a394ce65babde03ef16fa6eceacf2e864e
bksides-rtk <brandon.sides@rtktickets.com>
2024-09-04 13:32:06 -0400

    listings service interface

--- listings/listings.go
+++ listings/listings.go
@@ -7,12 +7,21 @@ import (
 	"github.com/rtk-tickets/example-app-global/models"
 )
 
+type ListingsService interface {
+	GetListings() []models.Listing
+}
+
+var ListingsServices []ListingsService = []ListingsService{
+	&gametime.GameTimeListingsService{},
+	&seatgeek.SeatGeekListingsService{},
+	&ticketmaster.TicketmasterListingsService{},
+}
+
 func GetListings() []models.Listing {
-	ret := gametime.GetListingsGametime()
-
-	ret = append(ret, seatgeek.GetListingsSeatgeek()...)
-
-	ret = append(ret, ticketmaster.GetListingsTicketmaster()...)
+	ret := []models.Listing{}
+	for _, service := range ListingsServices {
+		ret = append(ret, service.GetListings()...)
+	}
 
 	return ret
 }
