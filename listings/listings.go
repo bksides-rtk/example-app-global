bff55067c9643965fe0621ee391713529bd7f977
bksides-rtk <brandon.sides@rtktickets.com>
2024-09-04 13:39:41 -0400

    put listings svcs in struct

--- listings/listings.go
+++ listings/listings.go
@@ -1,18 +1,35 @@
 package listings
 
 import (
+	"github.com/rtk-tickets/example-app-global/listings/gametime"
+	"github.com/rtk-tickets/example-app-global/listings/seatgeek"
+	"github.com/rtk-tickets/example-app-global/listings/ticketmaster"
 	"github.com/rtk-tickets/example-app-global/models"
 )
+
+type multiListingsService struct {
+	marketplaceSvcs []ListingsService
+}
+
+func (mls *multiListingsService) GetListings() []models.Listing {
+	ret := []models.Listing{}
+	for _, service := range mls.marketplaceSvcs {
+		ret = append(ret, service.GetListings()...)
+	}
+
+	return ret
+}
 
 type ListingsService interface {
 	GetListings() []models.Listing
 }
 
-func GetListings(listingsServices []ListingsService) []models.Listing {
-	ret := []models.Listing{}
-	for _, service := range listingsServices {
-		ret = append(ret, service.GetListings()...)
+func InitListingsService() ListingsService {
+	return &multiListingsService{
+		marketplaceSvcs: []ListingsService{
+			&seatgeek.SeatGeekListingsService{},
+			&ticketmaster.TicketmasterListingsService{},
+			&gametime.GameTimeListingsService{},
+		},
 	}
-
-	return ret
 }
--- main.go
+++ main.go
@@ -3,23 +3,17 @@ package main
 import (
 	dbPkg "github.com/rtk-tickets/example-app-global/db"
 	"github.com/rtk-tickets/example-app-global/listings"
-	"github.com/rtk-tickets/example-app-global/listings/gametime"
-	"github.com/rtk-tickets/example-app-global/listings/seatgeek"
-	"github.com/rtk-tickets/example-app-global/listings/ticketmaster"
 	"github.com/rtk-tickets/example-app-global/logging"
 )
 
 func main() {
 	loggingService := logging.InitLoggingService()
 	dbService := dbPkg.InitDBService(loggingService)
+	listingsService := listings.InitListingsService()
 
 	dbService.DoThing1()
 
-	listings := listings.GetListings([]listings.ListingsService{
-		&gametime.GameTimeListingsService{},
-		&seatgeek.SeatGeekListingsService{},
-		&ticketmaster.TicketmasterListingsService{},
-	})
+	listings := listingsService.GetListings()
 
 	dbService.DoThing2(listings)
 }
